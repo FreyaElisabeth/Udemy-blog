bd5ea92191256c8cad80eac97698a82be8275491
Freya Gehrke <freya.gehrke@xing.com>
2019-04-24 14:17:06 +0200

    Implement API call via postsReducer

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,7 +4,23 @@ import { fetchPosts } from '../actions'
 
 class PostList extends React.Component {
   render() {
-    return <div>PostList</div>
+    return <div className="ui relaxed divided list">{this.renderList()}</div>
+  }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
   }
 
   componentDidMount() {
@@ -12,7 +28,11 @@ class PostList extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return { posts: state.posts }
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostList)
